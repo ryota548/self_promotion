9ad3e1f663feaff1a02e43d17cf15189f20c6961
ryota-ko <trb_right9@yahoo.co.jp>
2015-06-17 18:10:20 +0900

    Initial Commit

---
+++ self_promotion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  self_promotion
+//
+//  Created by ryota-ko on 2015/06/17.
+//  Copyright (c) 2015年 ryota-ko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
